4ff88403a8137ca941f14ebb473815449345577b
fadten <info@fadten.com>
2020-08-06 08:41:07 +0800

    Update smzdm_get_points.py

--- smzdm_get_points.py
+++ smzdm_get_points.py
@@ -39,18 +39,19 @@ if __name__ == "__main__":
     }
     data = req(current_url)    
     if data['checkin']['has_checkin']:
-        info = '%s ：%s 你目前积分：%s，经验值：%s，金币：%s，碎银子：%s，威望：%s，等级：%s，已经签到：%s天' % (data['sys_date'], data['nickname'], data['point'], data['exp'], data['gold'], data['silver'], data['prestige'], data['level'],data['checkin']['daily_checkin_num'])
+        info = '%s ：%s 你目前积分(第1次尝试签到)：%s，经验值：%s，金币：%s，碎银子：%s，威望：%s，等级：%s，已经签到：%s天' % (data['sys_date'], data['nickname'], data['point'], data['exp'], data['gold'], data['silver'], data['prestige'], data['level'],data['checkin']['daily_checkin_num'])
         print(info)
     else:
         time.sleep( 10 )
         data = req(current_url)
         if data['checkin']['has_checkin']:
-            info = '%s ：%s 你目前积分：%s，经验值：%s，金币：%s，碎银子：%s，威望：%s，等级：%s，已经签到：%s天' % (data['sys_date'], data['nickname'], data['point'], data['exp'], data['gold'], data['silver'], data['prestige'], data['level'],data['checkin']['daily_checkin_num'])
+            info = '%s ：%s 你目前积分(第2次尝试签到)：%s，经验值：%s，金币：%s，碎银子：%s，威望：%s，等级：%s，已经签到：%s天' % (data['sys_date'], data['nickname'], data['point'], data['exp'], data['gold'], data['silver'], data['prestige'], data['level'],data['checkin']['daily_checkin_num'])
             print(info)
         else:
             time.sleep( 10 )
-            data = req(current_url)            
-            info = '%s ：%s 你目前积分：%s，经验值：%s，金币：%s，碎银子：%s，威望：%s，等级：%s，已经签到：%s天' % (data['sys_date'], data['nickname'], data['point'], data['exp'], data['gold'], data['silver'], data['prestige'], data['level'],data['checkin']['daily_checkin_num'])
+            data = req(current_url)  
+            data['checkin']['has_checkin']:
+            info = '%s ：%s 你目前积分(第3次尝试签到)：%s，经验值：%s，金币：%s，碎银子：%s，威望：%s，等级：%s，已经签到：%s天' % (data['sys_date'], data['nickname'], data['point'], data['exp'], data['gold'], data['silver'], data['prestige'], data['level'],data['checkin']['daily_checkin_num'])
             print(info)
 
 
